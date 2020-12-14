54e7d5cf03d818817d0b9024074737867ee5aef9
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-14 19:07:56 +0100

    Frontend login functionality(no firebase) complete

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import { Link } from 'react-router-dom';
 
 function Login() {
-    return (
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = e => {
+        e.preventDevault();
+    }
+
+    const register = e => {
+        e.preventDevault();
+    }
+    
+    return ( 
         <div className = "login">
             <Link to = "/" >
                  <img className = "login__logo" alt="" src= "/images/calabash2.png" />
@@ -14,12 +26,14 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type = "text" />
+                    <input type = "text" value = {email} 
+                    onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type = "password" />
+                    <input type = "password" value = {password} 
+                    onChange = {e => setPassword(e.target.value)} />
 
-                    <button className = "login__signInButton">Sign In</button>
+                    <button type= "submit" onClick = {signIn} className = "login__signInButton">Sign In</button>
                 </form>
 
                 <p>
@@ -27,7 +41,7 @@ function Login() {
                     Please also do not see our Privacy Notice, Cookies Notice or Internet based ads notice because like our dating lives, they also do not exist.
                 </p>
 
-                <button className = "login__registerButton">Create Calabash Account</button>
+                <button type= "submit" onClick = {register} className = "login__registerButton">Create New Calabash Account</button>
             </div>
             
         </div>
